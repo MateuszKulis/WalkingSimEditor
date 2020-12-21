3334145216338052a4da675a8eca012fc276d125
MateuszKulis <mati22100@op.pl>
2020-12-21 11:47:38 +0100

    Add files via upload

---
+++ Data/Interaction/i_OpenDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WalkingSim.Inventory;
+
+namespace WalkingSim.Interactions
+{
+    public class i_OpenDoor : InteractableBase
+    {
+        public string setTriggerName = "Open";
+        public bool needKey;
+        public ItemScriptable keyItem;
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+
+            if (needKey)
+            {
+                if (InteractionController._isInventory)
+                {
+                    if (InteractionController._inventoryController.RemoveItem(keyItem))
+                    {
+                        Animator anim = GetComponent<Animator>();
+                        anim.SetTrigger(setTriggerName);
+                        isInteractable = false;
+                    }
+                }
+            }
+            else
+            {
+                Animator anim = GetComponent<Animator>();
+                anim.SetTrigger(setTriggerName);
+                isInteractable = false;
+            }
+        }
+
+    }
+}
---
+++ Data/Interaction/i_PickUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WalkingSim.Inventory;
+
+namespace WalkingSim.Interactions
+{
+    public class i_PickUp : InteractableBase
+    {
+        public ItemScriptable item;
+        public int amount;
+
+        public override void OnInteract()
+        {
+            base.OnInteract();
+            Destroy(gameObject);
+            if(InteractionController._isInventory)InteractionController._inventoryController.AddItem(item);
+        }
+    }
+}
